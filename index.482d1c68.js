86c433c2c506da15d0ecebbfc4bbce34ed316167
root <marti@martimcdee.com>
2022-08-13 21:18:41 +0000

    turned off debugging

--- index.482d1c68.js
+++ index.482d1c68.js
@@ -1406,7 +1406,7 @@ class InputHandler {
                 if (!this.keyPressed) this.game.sounds.playMusic();
                 this.keyPressed = true;
                 this.keys.push(e.key);
-            } else if (e.key === "d") this.game.debug = !this.game.debug;
+            } else if (e.key === "d") ;
             else if (e.key === "Enter" && this.game.gameOver) this.game.start();
             else if (e.key === "m") this.game.sounds.toggleMusic();
         });
